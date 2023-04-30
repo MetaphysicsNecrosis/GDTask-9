eae010e6c54e55992e6eb3f8fc1c0c07b841251f
MetaphysicsNecrosis <MetaphysicsNecrosis@DESKTOP-A9QI9P7>
2023-04-30 14:24:30 +0300

    Добавьте файлы проекта.

---
+++ GDTask-9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDTask_9
+{
+    internal class Program
+    {
+        public static String ROTik(String target)
+        {
+            String newS = "";
+            for (int i = 0; i < target.Length; i++)
+            {
+                if (Char.IsLetter(target[i]))
+                {
+                    if ((target[i] > 64 && target[i] < 78) ||
+                        (target[i] > 96 && target[i] < 110))
+                    {
+                        newS += ((char)(target[i] + 13));
+                    }
+                    else 
+                    {
+                        newS += ((char)(target[i] - 13));
+                    }
+                }
+                else newS += (target[i]);
+            }
+            return newS;
+        }
+
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            s = ROTik(s);
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
